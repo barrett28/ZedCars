eb595b0e75e598032ea1af02154c2aa1823e2477
Atharva Joshi <atharvajoshi664@gmail.com>
2025-11-05 12:09:58 +0530

    Done changes in the Home Controller for Purchase

--- ZedCars.Net8/Models/PurchaseRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace ZedCars.Net8.Models
-{
-    public class PurchaseRequest
-    {
-        public Purchase Purchase { get; set; } = new();
-        public List<string>? SelectedAccessories { get; set; }
-    }
-}
